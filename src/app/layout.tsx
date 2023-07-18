41226f76ff8741347e490cc7b18969b8b4b56a0d
João Pedro Carvalho <joaopedro.nc@outlook.com>
2023-07-18 18:14:22 +0000

    💄 style: Add Header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import './styles/global.css'
 import type { Metadata } from 'next'
 import { Roboto } from 'next/font/google'
 
+import { Header } from './components/Header'
+
 const roboto = Roboto({ weight: ['400', '700'], subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -17,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="pt-br">
       <body className={`${roboto.className} w-screen h-screen`}>
+        <Header />
         {children}
       </body>
     </html>
