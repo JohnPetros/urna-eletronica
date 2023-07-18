6e48b40df69f3847f77f1c3f624a079f337a8df0
João Pedro Carvalho <joaopedro.nc@outlook.com>
2023-07-18 18:46:43 +0000

    💄 style: Add input component

---
+++ src/app/components/Form/Input/index.tsx
@@ -0,0 +1,22 @@
+interface InputProps {
+  id: string
+  type: string
+  label: string
+  hasFocus?: boolean
+}
+
+export function Input({ id, type, label, hasFocus = false }: InputProps) {
+  return (
+    <div>
+      <label htmlFor={id} className="text-zinc-900 font-bold text-lg block">
+        {label}
+      </label>
+      <input
+        type={type}
+        id={id}
+        autoFocus={hasFocus}
+        className="border border-transparent mt-2 border-zinc-900 focus:border-blue-900 p-2 w-full transition-colors duration-200"
+      />
+    </div>
+  )
+}
