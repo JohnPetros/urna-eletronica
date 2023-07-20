cb53cfdb5dee71f5e4422cc53ed0d0c2264e7cad
João Pedro Carvalho <joaopedro.nc@outlook.com>
2023-07-20 13:57:47 +0000

    ➕ Add getStoragedUser util function

---
+++ src/functions/getStoragedUser.ts
@@ -0,0 +1,6 @@
+import { User } from '@/types/user'
+
+export function getStoragedUser() {
+  const storagedUser = localStorage.getItem('urna-eletronica@user')
+  return storagedUser ? (JSON.parse(storagedUser) as User) : null
+}
