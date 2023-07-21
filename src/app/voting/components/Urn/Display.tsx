3f57f674f437cf7bdcfd1ef8b61115195e23a5cc
João Pedro <joaopedro.nc@outlook.com>
2023-07-21 16:35:16 -0300

    ➕ Add Display component

--- src/app/voting/components/Urn/Display.tsx
+++ src/app/voting/components/Urn/Display.tsx
@@ -1,12 +1,14 @@
+import { Field } from "./Field";
+
 export function Display() {
   return (
-    <div className="bg-zinc-100 border border-zinc-800 p-3">
-      <strong className="uppercase ">Deputado federal</strong>
+    <div className="bg-zinc-100 border border-zinc-800 p-6">
+      <strong className="uppercase text-xl block pr-8">Deputado federal</strong>
 
-      <div className="flex gap-1">
-        <span className="border border-zinc-900 p-1">8</span>
-        <span className="border border-zinc-900 p-1">8</span>
-        <span className="border border-zinc-900 p-1">8</span>
+      <div className="flex gap-1 mt-4">
+        <Field number={8} />
+        <Field number={8} />
+        <Field number={8} />
       </div>
     </div>
   )
