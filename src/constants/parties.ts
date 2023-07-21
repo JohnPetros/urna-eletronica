9e65704474c6f9ec5d8fdcbb8093bbc0f3f5cb5c
João Pedro <joaopedro.nc@outlook.com>
2023-07-21 17:41:42 -0300

    🚚 Add parties constant

---
+++ src/constants/parties.ts
@@ -0,0 +1,24 @@
+import { Party } from "@/types/party";
+
+export const PARTIES: Party[] = [
+  {
+    title: 'Partido dos Esportes',
+    abbr: 'PEsp',
+  },
+  {
+    title: 'Partido dos Músicos',
+    abbr: 'PMus',
+  },
+  {
+    title: 'Partido das Profissões',
+    abbr: 'PProf',
+  },
+  {
+    title: 'Partido das Festas',
+    abbr: 'PFest',
+  },
+  {
+    title: 'Partido do Folclore',
+    abbr: 'PFolc',
+  },
+]
