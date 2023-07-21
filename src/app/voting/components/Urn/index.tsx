60d6d0bcf49b39771bf0ad39177bcf7c614b8a47
João Pedro <joaopedro.nc@outlook.com>
2023-07-21 19:46:47 -0300

    💄 style: Fix some margin/padding values

--- src/app/voting/components/Urn/index.tsx
+++ src/app/voting/components/Urn/index.tsx
@@ -3,8 +3,9 @@ import { Display } from './Display'
 import { Keyboard } from './Keyboard'
 
 export function Urn() {
+
   return (
-    <div className="bg-zinc-300 w-[800px] grid grid-cols-[1fr_340px] gap-6 border border-zinc-100 p-8">
+    <div className="bg-zinc-300 w-[800px] grid grid-cols-[1fr_320px] gap-6 border border-zinc-100 p-8">
       <Display />
       <Keyboard />
     </div>
