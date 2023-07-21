4d435d7524c19da0629b54d564e770581999e472
João Pedro <joaopedro.nc@outlook.com>
2023-07-21 19:46:05 -0300

    💄 style: Add Header component to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import { Header } from './components/Header'
 import { Form } from './components/Form'
 
 export default function Home() {
   return (
-    <main className="flex h-[calc(100%-80px)] items-center justify-center">
-      <Form />
-    </main>
+    <>
+      <Header />
+      <main className="flex h-[calc(100%-80px)] items-center justify-center">
+        <Form />
+      </main>
+    </>
   )
 }
