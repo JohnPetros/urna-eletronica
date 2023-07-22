37e4cf05a1ca04cba685ef6ecbec847062cacad2
João Pedro <joaopedro.nc@outlook.com>
2023-07-22 11:22:29 -0300

    ✨ feat: Disable Key on full digits

--- src/app/voting/components/Urn/Key.tsx
+++ src/app/voting/components/Urn/Key.tsx
@@ -1,23 +1,37 @@
 'use client'
 import { twMerge } from 'tailwind-merge'
-import { motion } from 'framer-motion'
+import { Variants, motion } from 'framer-motion'
 import { useUrn } from '@/hooks/useUrn'
 
 interface KeyProps {
   value: string
+  isAction?: boolean
   className?: string
 }
 
-export function Key({ value, className }: KeyProps) {
-  const { handleKeyPress } = useUrn()
+export function Key({ value, isAction = false, className }: KeyProps) {
+  const { state, dispatch } = useUrn()
+  const isEnable = state.canPressKey || isAction
+
+  const keyVariants: Variants = {
+    tap: {
+      scale: 0.95,
+    },
+  }
 
   return (
     <motion.button
-      onClick={() => handleKeyPress(value.toLowerCase())}
-      whileTap={{ scale: 0.95 }}
+      onClick={() => {
+        if (isEnable) {
+          dispatch({ type: 'pressKey', payload: value.toLowerCase() })
+        }
+      }}
+      variants={keyVariants}
+      whileTap={isEnable ? 'tap' : ''}
       className={twMerge(
         'text-zinc-100 rounded-lg border-b-2 border-zinc-400 uppercase font-bold',
-        className
+        className,
+        isEnable ? 'cursor-pointer' : 'cursor-not-allowed'
       )}
     >
       {value}
