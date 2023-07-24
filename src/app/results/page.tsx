42b6b373b26da6556320f61cddf2faa2f6d7f0cc
João Pedro <joaopedro.nc@outlook.com>
2023-07-24 16:20:11 -0300

    ✨ feat: Add back button to Results page

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -1,12 +1,14 @@
 'use client'
 import { useUrn } from '@/hooks/useUrn'
-import { Vote } from './components/Vote'
+import { useRouter } from 'next/navigation'
+
 import { ROLES_TITLES } from '@/constants/roles-titles'
-import Link from 'next/link'
+
+import { Vote } from './components/Vote'
+
 import { Variants, motion } from 'framer-motion'
 import { blinkVariants } from '../voting/components/Urn/Display'
 import { getStoragedUser } from '@/functions'
-import { useRouter } from 'next/navigation'
 
 const linkVariants: Variants = {
   hidden: {
@@ -112,22 +114,52 @@ const mock = [
       },
     ],
   },
+  {
+    number: '951',
+    name: 'Saci-Pererê',
+    party: 'PFolc',
+    alternates: ['Caipora', 'Mãe do Ouro'],
+    images: [
+      {
+        url: 'https://www.tse.jus.br/hotsites/simulador-de-votacao/image/figuras/135x145/24bpp/951_saci.jpg',
+        caption: 'Senador',
+      },
+      {
+        url: 'https://www.tse.jus.br/hotsites/simulador-de-votacao/image/figuras/95x105/24bpp/951_caipora.jpg',
+        caption: '1º Suplente',
+        small: true,
+      },
+      {
+        url: 'https://www.tse.jus.br/hotsites/simulador-de-votacao/image/figuras/95x105/24bpp/951_mae_douro.jpg',
+        caption: '2° Suplente',
+        small: true,
+      },
+    ],
+  },
 ]
 
 export default function Results() {
   const { state } = useUrn()
   const router = useRouter()
+  const storagedUser = getStoragedUser()
 
-  if (!state.votedCandidates.length) {
-    router.push('/voting')
-    return null
+  function handleExitClick() {
+    localStorage.removeItem('urna-eletronica@user')
+    router.push('/')
   }
+
+  // if (!state.votedCandidates.length || !storagedUser) {
+  //   router.push('/voting')
+  //   return null
+  // }
 
   return (
     <div className="bg-blue-900 h-screen flex flex-col items-center">
-      <h2 className="text-zinc-100 text-2xl mt-8">Seus votos</h2>
-      <dl className="flex flex-col gap-3">
-        {state.votedCandidates.map((candidate, index) => (
+      <h2 className="text-zinc-100 text-2xl mt-6">
+        Seus votos, {storagedUser.name}:
+      </h2>
+      <dl className="flex flex-col gap-6 mt-6">
+        {mock.map((candidate, index) => (
           <Vote
             role={ROLES_TITLES[index]}
             candidate={candidate}
@@ -136,20 +168,16 @@ export default function Results() {
         ))}
       </dl>
 
-      <Link
-        href="/"
-        className="mt-8 text-zinc-100 uppercase font-semibold text-3xl tracking-wide"
+      <motion.button
+        variants={blinkVariants}
+        animate={'blink'}
+        className="mt-8 text-zinc-100 bg-transparent uppercase font-semibold text-3xl tracking-wide"
+        onClick={handleExitClick}
       >
-        <motion.div variants={blinkVariants} animate={'blink'}>
-          <motion.span
-            variants={linkVariants}
-            initial="hidden"
-            animate="visible"
-          >
-            Sair
-          </motion.span>
-        </motion.div>
-      </Link>
+        <motion.span variants={linkVariants} initial="hidden" animate="visible">
+          Sair
+        </motion.span>
+      </motion.button>
     </div>
   )
 }
