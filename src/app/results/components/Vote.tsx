e051e250d40f7b5390b2a2779e63d55d9a4348f3
João Pedro <joaopedro.nc@outlook.com>
2023-07-24 16:19:36 -0300

    💄 style: Add card hover to cadidate alternates

--- src/app/results/components/Vote.tsx
+++ src/app/results/components/Vote.tsx
@@ -2,6 +2,8 @@ import { RoleTitle } from '@/hooks/useUrn'
 import Image from 'next/image'
 import { Variants, motion } from 'framer-motion'
 import type { Candidate } from '@/types/candidate'
+
+import * as HoverCard from '@radix-ui/react-hover-card'
 interface VoteProps {
   role: RoleTitle
   candidate: Candidate | null
@@ -48,12 +50,12 @@ export function Vote({ role, candidate, index }: VoteProps) {
       {candidate ? (
         <motion.dd
           variants={candidateVariants}
-          className="flex items-center w-96 gap-2 mt-6 relative"
+          className="flex items-center w-96 gap-2 relative"
         >
           <Image
             src={candidate.images[0].url}
-            width={64}
-            height={64}
+            width={48}
+            height={48}
             alt={candidate.name}
           />
           <dl>
@@ -74,14 +76,33 @@ export function Vote({ role, candidate, index }: VoteProps) {
             <ul className="flex gap-2">
               {candidate.alternates.map((alternate, index) => (
                 <li>
-                  <Image
-                    src={candidate.images[index + 1].url}
-                    width={40}
-                    height={40}
-                    alt={alternate}
-                  />
-                  {/* <small>{alternate}</small>
-                  <small>{candidate.images[index + 1].caption}</small> */}
+                  <HoverCard.Root openDelay={200}>
+                    <HoverCard.Trigger>
+                      <Image
+                        src={candidate.images[index + 1].url}
+                        width={40}
+                        height={40}
+                        alt={alternate}
+                      />
+                    </HoverCard.Trigger>
+
+                    <HoverCard.Portal>
+                      <HoverCard.Content
+                        className="bg-zinc-100 p-1 rounded"
+                        sideOffset={5}
+                      >
+                        <HoverCard.Arrow className="fill-zinc-100" />
+                        <div className="flex flex-col">
+                          <strong className="text-zinc-800 text-xs">
+                            {alternate}
+                          </strong>
+                          <small className="text-zinc-800 text-xs">
+                            {candidate.images[index + 1].caption}
+                          </small>
+                        </div>
+                      </HoverCard.Content>
+                    </HoverCard.Portal>
+                  </HoverCard.Root>
                 </li>
               ))}
             </ul>
