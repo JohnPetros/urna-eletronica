a569d6601837da50080a7a967fd134ab96da57ac
João Pedro <joaopedro.nc@outlook.com>
2023-08-05 17:04:38 -0300

    🧪 test Modal component

--- src/app/components/Modal/Modal.test.tsx
+++ src/app/components/Modal/Modal.test.tsx
@@ -1,28 +1,37 @@
 import { act, render, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { Modal, ModalRef } from '.'
-import { RefObject, createRef } from 'react'
+import { Modal, ModalRef, ModalType } from '.'
+import { createRef } from 'react'
+import { UserContext } from '@/hooks/useUser'
 
-jest.mock('next/navigation', () => {
-  return {
-    useRouter() {
-      return {
-        router: { push: jest.fn() },
-      }
-    },
-  }
-})
+const mockedPush = jest.fn()
 
-const renderModal = () => {
+jest.mock('next/navigation', () => ({
+  useRouter() {
+    return { push: mockedPush }
+  },
+}))
+
+interface RenderModalParams {
+  type: ModalType
+  hasUser?: boolean
+}
+
+const renderModal = ({
+  type = 'success',
+  hasUser = false,
+}: RenderModalParams) => {
   const modalRef = createRef<ModalRef>()
 
   render(
-    <Modal
-      ref={modalRef}
-      type="success"
-      title="Mocked title"
-      text="Mocked text"
-    />
+    <UserContext.Provider value={{ hasUser } as any}>
+      <Modal
+        ref={modalRef}
+        type={type}
+        title="Mocked title"
+        text="Mocked text"
+      />
+    </UserContext.Provider>
   )
 
   return { modalRef }
@@ -30,7 +39,7 @@ const renderModal = () => {
 
 describe('Modal component', () => {
   it('should open', async () => {
-    const { modalRef } = renderModal()
+    const { modalRef } = renderModal({ type: 'success' })
 
     act(() => {
       modalRef.current?.open()
@@ -44,7 +53,7 @@ describe('Modal component', () => {
   })
 
   it('should close', async () => {
-    const { modalRef } = renderModal()
+    const { modalRef } = renderModal({ type: 'success' })
 
     act(() => {
       modalRef.current?.open()
@@ -57,6 +66,52 @@ describe('Modal component', () => {
       expect(screen.queryByRole('dialog')).not.toBeInTheDocument()
       expect(screen.queryByText('Mocked title')).not.toBeInTheDocument()
       expect(screen.queryByText('Mocked text')).not.toBeInTheDocument()
+    })
+  })
+
+  it('should not redirect user to voting page when modal type is not success and has user', async () => {
+    const { modalRef } = renderModal({ type: 'error', hasUser: true })
+
+    act(() => {
+      modalRef.current?.open()
+    })
+
+    const button = screen.getByRole('button')
+    await userEvent.click(button)
+
+    await waitFor(() => {
+      expect(mockedPush).not.toHaveBeenCalled()
+    })
+  })
+
+  it('should not redirect user to voting page when modal type is success and has no user', async () => {
+    const { modalRef } = renderModal({ type: 'error', hasUser: false })
+
+    act(() => {
+      modalRef.current?.open()
+    })
+
+    const button = screen.getByRole('button')
+    await userEvent.click(button)
+
+    await waitFor(() => {
+      expect(mockedPush).not.toHaveBeenCalled()
+    })
+  })
+
+  it('should redirect user to voting page when modal type is success and has user', async () => {
+    const { modalRef } = renderModal({ type: 'success', hasUser: true })
+
+    act(() => {
+      modalRef.current?.open()
+    })
+
+    const button = screen.getByRole('button')
+    await userEvent.click(button)
+
+    await waitFor(() => {
+      expect(mockedPush).toHaveBeenCalled()
+      expect(mockedPush).toHaveBeenCalledWith('/voting')
     })
   })
 })
