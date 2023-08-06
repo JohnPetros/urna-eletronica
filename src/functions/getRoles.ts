7fdb6b30a8ee07258f95230d0fa965a25d98270e
João Pedro <joaopedro.nc@outlook.com>
2023-08-06 15:21:24 -0300

    ➕ Add getRoles util function

--- src/functions/getRoles.ts
+++ src/functions/getRoles.ts
@@ -1,5 +1,7 @@
-export async function getRoles() {
-  const response = await fetch('http://localhost:3004/roles')
+import { Role } from "@/types/role"
+
+export async function getRoles(): Promise<Role[]> {
+  const response = await fetch('http://localhost:3001/roles')
   const roles = response.json()
   return roles
 }
