99ea6025ccd1c591e7e2d6af2d731be87769199f
João Pedro <joaopedro.nc@outlook.com>
2023-08-06 15:27:32 -0300

    ➕ Add getCurrentDate to Form test

--- src/app/components/Form/Form.test.tsx
+++ src/app/components/Form/Form.test.tsx
@@ -4,18 +4,7 @@ import { Form } from '.'
 import { ModalContext } from '../../../hooks/useModal'
 import { UserContext } from '@/hooks/useUser'
 import { Modal } from '../Modal'
-
-function getCurrentDate(daysToAdd = 0) {
-  const currentDate = new Date()
-  currentDate.setDate(currentDate.getDate() + daysToAdd)
-
-  const year = currentDate.getFullYear()
-  const month = String(currentDate.getMonth() + 1).padStart(2, '0')
-  const day = String(currentDate.getDate()).padStart(2, '0')
-
-  const formattedDate = `${year}-${month}-${day}`
-  return formattedDate
-}
+import { getCurrentDate } from '@/functions'
 
 jest.mock('next/navigation', () => {
   return {
