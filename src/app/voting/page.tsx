40df024999c14bd394a11d15a5666fad799d9cad
João Pedro <joaopedro.nc@outlook.com>
2023-08-06 15:27:59 -0300

    ➕ Add getRoles to Voting page

--- src/app/voting/page.tsx
+++ src/app/voting/page.tsx
@@ -1,15 +1,22 @@
 import { getRoles } from '@/functions'
 import { Parties } from './components/Parties'
 import { Urn } from './components/Urn'
+import { Role } from '@/types/role'
 
 export default async function Voting() {
-  const roles = await getRoles()
+  let roles: Role[] = []
+
+  try {
+    roles = await getRoles()
+  } catch (error) {
+    throw new Error('Error ao buscar cargos da API')
+  }
 
   return (
     <div className="flex flex-col h-[calc(100%-80px)]">
       <Parties roles={roles} />
       <div className="h-full flex items-center justify-center">
-        <Urn />
+        <Urn roles={roles} />
       </div>
     </div>
   )
