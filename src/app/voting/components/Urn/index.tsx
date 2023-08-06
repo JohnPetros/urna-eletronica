39767eb1b3fe5e33bb91900a16ffd120bd058757
João Pedro <joaopedro.nc@outlook.com>
2023-08-06 15:26:09 -0300

    ♻️ Add Lifting State Up to Urn component

--- src/app/voting/components/Urn/index.tsx
+++ src/app/voting/components/Urn/index.tsx
@@ -1,10 +1,12 @@
 import { Display } from './Display'
 import { Keyboard } from './Keyboard'
-import { getRoles } from '@/functions'
+import { Role } from '@/types/role'
 
-export async function Urn() {
-  const roles = await getRoles()
+interface UrnProps {
+  roles: Role[]
+}
 
+export function Urn({ roles }: UrnProps) {
   return (
     <div className="bg-zinc-300 w-[900px] grid grid-cols-1 md:grid-cols-[1fr_320px] gap-6 border border-zinc-100 p-8 mt-48 md:mt-0">
       <Display roles={roles} />
