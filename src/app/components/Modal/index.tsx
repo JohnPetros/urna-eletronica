7025136468a4568eb96f0477a02c76fc8204394c
João Pedro <joaopedro.nc@outlook.com>
2023-08-25 13:29:35 -0300

    ♻️ refactor: Remove action functions from Modal component

--- src/app/components/Modal/index.tsx
+++ src/app/components/Modal/index.tsx
@@ -49,91 +49,60 @@ const animations: Animation = {
 export type ModalType = 'error' | 'warning' | 'success'
 
 export interface ModalProps {
+  isOpen: boolean
   type: ModalType
   title: string
   text: string
+  onClick: VoidFunction
 }
 
-export interface ModalRef {
-  open: VoidFunction
-  close: VoidFunction
+export function Modal({ isOpen, title, text, type, onClick }: ModalProps) {
+
+  return (
+    <Dialog.Root open={isOpen}>
+      <Dialog.Portal>
+        <Dialog.Overlay className="bg-black/70 fixed inset-0" />
+        <Dialog.Content
+          data-testid={type}
+          className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[90vw] max-w-[450px]"
+        >
+          <AnimatePresence>
+            <motion.div
+              role="alertdialog"
+              variants={variants}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              className=" bg-white p-8 rounded shadow-sm flex flex-col gap-6 items-center w-full"
+            >
+              <Lottie
+                autoplay
+                animationData={animations[type]}
+                loop={false}
+                style={{ width: 80, height: 80 }}
+              />
+              <div>
+                <Dialog.Title className="block text-xl text-center text-zinc-900">
+                  {title}
+                </Dialog.Title>
+                <Dialog.Description className="block text-center text-lg mt-4">
+                  {text}
+                </Dialog.Description>
+              </div>
+
+              <Dialog.Close asChild>
+                <button
+                  className="bg-blue-600 text-zinc-100 text-lg text-center outline-4  outline-offset-4 outline-blue-400  py-2 px-6 rounded"
+                  aria-label="Fechar modal"
+                  onClick={onClick}
+                >
+                  Ok
+                </button>
+              </Dialog.Close>
+            </motion.div>
+          </AnimatePresence>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
+  )
 }
-
-export const Modal = forwardRef<ModalRef, ModalProps>(
-  ({ title, text, type }, ref) => {
-    const [isOpen, setisOpen] = useState(false)
-    const { hasUser } = useUser()
-    const router = useRouter()
-
-    useImperativeHandle(ref, () => {
-      return {
-        open,
-        close,
-      }
-    })
-
-    function open() {
-      setisOpen(true)
-    }
-
-    function close() {
-      setisOpen(false)
-    }
-
-    function handleButtonClick() {
-      if (type === 'success' && hasUser) {
-        router.push('/voting')
-      }
-
-      setisOpen(false)
-    }
-
-    return (
-      <Dialog.Root open={isOpen}>
-        <Dialog.Portal>
-          <Dialog.Overlay className="bg-black/70 fixed inset-0" />
-          <Dialog.Content
-            data-testid={type}
-            className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[90vw] max-w-[450px]"
-          >
-            <AnimatePresence>
-              <motion.div
-                role="alertdialog"
-                variants={variants}
-                initial="hidden"
-                animate="visible"
-                exit="exit"
-                className=" bg-white p-8 rounded shadow-sm flex flex-col gap-6 items-center w-full"
-              >
-                <Lottie
-                  autoplay
-                  animationData={animations[type]}
-                  loop={false}
-                  style={{ width: 80, height: 80 }}
-                />
-                <div>
-                  <Dialog.Title className="block text-xl text-center text-zinc-900">
-                    {title}
-                  </Dialog.Title>
-                  <Dialog.Description className="block text-center text-lg mt-4">
-                    {text}
-                  </Dialog.Description>
-                </div>
-
-                <Dialog.Close asChild>
-                  <button
-                    className="bg-blue-600 text-zinc-100 text-lg text-center outline-4  outline-offset-4 outline-blue-400  py-2 px-6 rounded"
-                    aria-label="Fechar modal"
-                    onClick={handleButtonClick}
-                  >
-                    Ok
-                  </button>
-                </Dialog.Close>
-              </motion.div>
-            </AnimatePresence>
-          </Dialog.Content>
-        </Dialog.Portal>
-      </Dialog.Root>
-    )
-  }
-)
