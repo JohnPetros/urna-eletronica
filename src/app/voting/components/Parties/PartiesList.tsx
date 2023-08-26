983aaf1377125e918068a28732c9a56feda6b269
João Pedro <joaopedro.nc@outlook.com>
2023-08-26 20:58:01 -0300

    ➕ Add PartiesList component

---
+++ src/app/voting/components/Parties/PartiesList.tsx
@@ -0,0 +1,37 @@
+import { Party } from '@/types/party'
+import * as Tabs from '@radix-ui/react-tabs'
+
+
+interface PartiesListProps {
+  parties: Party[]
+  onClick: (abbr: string) => void
+}
+
+export function PartiesList({ parties, onClick }: PartiesListProps) {
+  return (
+    <Tabs.List
+      className="p-6 md:p-0 flex flex-col md:flex-row bg-blue-900 items-center md:justify-center justify-start gap-6 border-t border-zinc-300"
+      role="tab-list"
+      aria-label="Lista de partidos"
+    >
+      {parties.map(({ title, abbr }) => {
+        return (
+          <Tabs.Trigger
+            key={abbr}
+            value={`tab-${abbr}`}
+            role="tab"
+            data-testid={`tab-${abbr}`}
+            id={`tab-${abbr}`}
+            className=" flex flex-col gap-1 items-center py-7 px-2 font-medium hover:text-white transition-colors duration-200 cursor-pointer"
+            aria-controls={`tab-partido-${abbr}`}
+            onClick={() => onClick(abbr)}
+            // onClick={() => handlePartyClick(abbr)}
+          >
+            <strong>{abbr}</strong>
+            <small className="uppercase">{title}</small>
+          </Tabs.Trigger>
+        )
+      })}
+    </Tabs.List>
+  )
+}
