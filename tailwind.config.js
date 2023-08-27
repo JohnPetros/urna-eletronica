77984b73fdb61532a578bfee65a9f9dc66f96831
João Pedro <joaopedro.nc@outlook.com>
2023-08-27 09:28:18 -0300

    🔧 chore: Add break points to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,15 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: '424px',
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1280px',
+      },
+    },
   },
   plugins: [],
 }
