93108dfa7f9323384c77a0061598cc0cc49f97e2
João Pedro <joaopedro.nc@outlook.com>
2023-08-27 09:30:31 -0300

    📱 Adapt Party component to mobile

--- src/app/voting/components/Parties/Party.tsx
+++ src/app/voting/components/Parties/Party.tsx
@@ -40,18 +40,32 @@ export function Party({
       variants={partyVariants}
       initial="initial"
       animate="entry"
-      className="flex items-center justify-center gap-3 md:gap-6 py-4 text-zinc-100"
-      role='tabpanel'
+      className="flex items-center justify-center flex-col md:flex-row gap-3 md:gap-8 py-4 text-zinc-100 fixed h-full left-0 md:static bg-blue-900 w-52 md:w-full z-30"
+      role="tabpanel"
       id={`tab-partido-${abbr}`}
       aria-labelledby={`tab-${abbr}`}
     >
-      <span className="text-2xl font-bold">{number}</span>
-      <div>
-        <div className="flex flex-col">
-          <span>{abbr}</span>
-          <span>{title}</span>
+      <div className="flex flex-col md:flex-row gap-6">
+        <div className='flex justify-between items-center'>
+          <span className="text-2xl font-bold">{number}</span>
+          <motion.button
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.25 }}
+            onClick={handleCloseClick}
+            className="md:hidden p-2 self-start text-3xl -mt-2"
+            aria-label="Parar de ver os candidatos desse partido"
+          >
+            ×
+          </motion.button>
         </div>
-        <strong className="block mt-6">{state.activeRoleTitle}</strong>
+
+        <div>
+          <div className="flex flex-col">
+            <span>{abbr}</span>
+            <span>{title}</span>
+          </div>
+          <strong className="block mt-6">{state.activeRoleTitle}</strong>
+        </div>
       </div>
 
       {candidates.map(({ name, images, number, alternates }) => (
@@ -68,7 +82,8 @@ export function Party({
         whileHover={{ scale: 1.1 }}
         transition={{ duration: 0.25 }}
         onClick={handleCloseClick}
-        className="p-2 self-start text-3xl"
+        className="p-2 self-start text-3xl hidden md:block"
+        aria-label="Parar de ver os candidatos desse partido"
       >
         ×
       </motion.button>
