ca77148a5218dacaec564fedc67cb6e213d8ab1d
João Pedro <joaopedro.nc@outlook.com>
2023-08-27 17:17:32 -0300

    🐛 fix: REsolve input component forwarded ref

--- src/app/components/Form/Input/index.tsx
+++ src/app/components/Form/Input/index.tsx
@@ -1,28 +1,35 @@
 'use client'
-import { InputHTMLAttributes, forwardRef, useId } from 'react'
+import { ForwardedRef, InputHTMLAttributes, forwardRef, useId } from 'react'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string
   error?: string
 }
 
-export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ label, error, ...rest }, ref) => {
-    const id = useId()
+function InputComponent(
+  { label, error, ...rest }: InputProps,
+  ref: ForwardedRef<HTMLInputElement>
+) {
+  const id = useId()
 
-    return (
-      <div>
-        <label htmlFor={id} className="text-zinc-900 font-bold text-lg block">
-          {label}
-        </label>
-        <input
-          ref={ref}
-          id={id}
-          className="border border-transparent mt-2 border-zinc-900 focus:border-blue-900 p-2 w-full transition-colors duration-200 rounded"
-          {...rest}
-        />
-        {error && <strong role="alert" className="text-red-600 font-normal">{error}</strong>}
-      </div>
-    )
-  }
-)
+  return (
+    <div>
+      <label htmlFor={id} className="text-zinc-900 font-bold text-lg block">
+        {label}
+      </label>
+      <input
+        ref={ref}
+        id={id}
+        className="border border-transparent mt-2 border-zinc-900 focus:border-blue-900 p-2 w-full transition-colors duration-200 rounded"
+        {...rest}
+      />
+      {error && (
+        <strong role="alert" className="text-red-600 font-normal">
+          {error}
+        </strong>
+      )}
+    </div>
+  )
+}
+
+export const Input = forwardRef(InputComponent)
