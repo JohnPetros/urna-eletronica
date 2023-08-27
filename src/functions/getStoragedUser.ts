ae6344ef4360946c5a2dd0d80e25def73c0f6871
João Pedro <joaopedro.nc@outlook.com>
2023-08-27 17:05:35 -0300

    🐛 fix: Resolve getStoragedUser function

--- src/functions/getStoragedUser.ts
+++ src/functions/getStoragedUser.ts
@@ -7,6 +7,4 @@ export function getStoragedUser() {
     const storagedUser = localStorage.getItem('urna-eletronica@user')
     return storagedUser ? (JSON.parse(storagedUser) as User) : null
   }
-
-  return null
 }
