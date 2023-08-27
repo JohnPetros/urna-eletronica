bce7b057030cee69dfa1f8786f1fd69c0d43b437
João Pedro <joaopedro.nc@outlook.com>
2023-08-27 09:30:00 -0300

    📱 Adapt Urn component to mobile

--- src/app/voting/components/Urn/index.tsx
+++ src/app/voting/components/Urn/index.tsx
@@ -8,7 +8,7 @@ interface UrnProps {
 
 export function Urn({ roles }: UrnProps) {
   return (
-    <div className="bg-zinc-300 w-[900px] grid grid-cols-1 md:grid-cols-[1fr_320px] gap-6 border border-zinc-100 p-8 mt-48 md:mt-0">
+    <div className="bg-zinc-300 w-[900px] grid grid-cols-1 md:grid-cols-[1fr_320px] gap-6 border border-zinc-100 p-8 mt-20 md:mt-0">
       <Display roles={roles} />
       <Keyboard />
     </div>
