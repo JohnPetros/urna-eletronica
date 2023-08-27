1bcc0c219c41600562b282a434b20dd1be51a168
João Pedro <joaopedro.nc@outlook.com>
2023-08-27 12:14:38 -0300

    📱 Adapt Parties list to mobile

--- src/app/voting/components/Parties/PartiesList.tsx
+++ src/app/voting/components/Parties/PartiesList.tsx
@@ -1,7 +1,6 @@
 import { Party } from '@/types/party'
 import * as Tabs from '@radix-ui/react-tabs'
 
-
 interface PartiesListProps {
   parties: Party[]
   onClick: (abbr: string) => void
@@ -10,7 +9,7 @@ interface PartiesListProps {
 export function PartiesList({ parties, onClick }: PartiesListProps) {
   return (
     <Tabs.List
-      className="p-6 md:p-0 flex flex-col md:flex-row bg-blue-900 items-center md:justify-center justify-start gap-6 border-t border-zinc-300"
+      className="p-6 md:p-0 flex flex-col md:flex-row bg-blue-900 items-center md:justify-center justify-start md:gap-6 border-t border-zinc-300 z-30"
       role="tab-list"
       aria-label="Lista de partidos"
     >
@@ -22,7 +21,7 @@ export function PartiesList({ parties, onClick }: PartiesListProps) {
             role="tab"
             data-testid={`tab-${abbr}`}
             id={`tab-${abbr}`}
-            className=" flex flex-col gap-1 items-center py-7 px-2 font-medium hover:text-white transition-colors duration-200 cursor-pointer"
+            className="flex flex-col gap-1 items-center py-7 px-2 font-medium hover:text-white transition-colors duration-200 cursor-pointer"
             aria-controls={`tab-partido-${abbr}`}
             onClick={() => onClick(abbr)}
             // onClick={() => handlePartyClick(abbr)}
