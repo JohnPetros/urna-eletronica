a303e567eaf937d93d69ace01a3960653b4ce809
João Pedro <joaopedro.nc@outlook.com>
2023-08-27 12:16:18 -0300

    📱Adapt Party component to mobile

--- src/app/voting/components/Parties/Party.tsx
+++ src/app/voting/components/Parties/Party.tsx
@@ -4,15 +4,18 @@ import { Candidate } from './Candidate'
 import type { Party } from '@/types/party'
 
 import { Variants, motion } from 'framer-motion'
+import { CloseTabButton } from './CloseTabButton'
 
 interface PartyProps {
   data: Party
-  onClose: VoidFunction
+  onPartyClose: VoidFunction
+  onTabListClose: VoidFunction
 }
 
 export function Party({
   data: { title, abbr, number, candidates },
-  onClose,
+  onPartyClose,
+  onTabListClose,
 }: PartyProps) {
   const { state } = useUrn()
 
@@ -31,62 +34,50 @@ export function Party({
     },
   }
 
-  function handleCloseClick() {
-    onClose()
-  }
-
   return (
     <motion.div
       variants={partyVariants}
       initial="initial"
       animate="entry"
-      className="flex items-center justify-center flex-col md:flex-row gap-3 md:gap-8 py-4 text-zinc-100 fixed h-full left-0 md:static bg-blue-900 w-52 md:w-full z-30"
+      className=" md:gap-8 text-zinc-100 fixed left-0 md:static h-screen w-full z-30 grid grid-cols-[13rem_2fr] justify-center items-center md:flex md:py-2 overflow-y-auto"
       role="tabpanel"
       id={`tab-partido-${abbr}`}
       aria-labelledby={`tab-${abbr}`}
     >
-      <div className="flex flex-col md:flex-row gap-6">
-        <div className='flex justify-between items-center'>
-          <span className="text-2xl font-bold">{number}</span>
-          <motion.button
-            whileHover={{ scale: 1.1 }}
-            transition={{ duration: 0.25 }}
-            onClick={handleCloseClick}
-            className="md:hidden p-2 self-start text-3xl -mt-2"
-            aria-label="Parar de ver os candidatos desse partido"
-          >
-            ×
-          </motion.button>
+      <div className="md:w-full h-full flex flex-col justify-center items-center md:items-start md:flex-row gap-3 bg-blue-900">
+        <div className="flex flex-col md:flex-row gap-6 py-4">
+          <div className="flex justify-between items-center">
+            <span className="text-2xl font-bold">{number}</span>
+            <div className="md:hidden">
+              <CloseTabButton onClose={onPartyClose} />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex flex-col">
+              <span>{abbr}</span>
+              <span>{title}</span>
+            </div>
+            <strong className="block mt-6">{state.activeRoleTitle}</strong>
+          </div>
         </div>
 
-        <div>
-          <div className="flex flex-col">
-            <span>{abbr}</span>
-            <span>{title}</span>
-          </div>
-          <strong className="block mt-6">{state.activeRoleTitle}</strong>
+        {candidates.map(({ name, images, number, alternates }) => (
+          <Candidate
+            key={number}
+            name={name}
+            images={images}
+            number={number}
+            alternates={alternates}
+          />
+        ))}
+
+        <div className="hidden md:block">
+          <CloseTabButton onClose={onPartyClose} />
         </div>
       </div>
 
-      {candidates.map(({ name, images, number, alternates }) => (
-        <Candidate
-          key={number}
-          name={name}
-          images={images}
-          number={number}
-          alternates={alternates}
-        />
-      ))}
-
-      <motion.button
-        whileHover={{ scale: 1.1 }}
-        transition={{ duration: 0.25 }}
-        onClick={handleCloseClick}
-        className="p-2 self-start text-3xl hidden md:block"
-        aria-label="Parar de ver os candidatos desse partido"
-      >
-        ×
-      </motion.button>
+      <div data-testid="party-tab-overlay" className="block h-full w-fullmd:hidden" onClick={onTabListClose}></div>
     </motion.div>
   )
 }
