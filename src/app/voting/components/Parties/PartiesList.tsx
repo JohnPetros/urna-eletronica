bc01eda6a08a178d1adec3aca32f97eba65a9f9e
João Pedro <joaopedro.nc@outlook.com>
2023-08-27 13:59:45 -0300

    ♿ Adjust tab list roles

--- src/app/voting/components/Parties/PartiesList.tsx
+++ src/app/voting/components/Parties/PartiesList.tsx
@@ -10,7 +10,6 @@ export function PartiesList({ parties, onClick }: PartiesListProps) {
   return (
     <Tabs.List
       className="p-6 md:p-0 flex flex-col md:flex-row bg-blue-900 items-center md:justify-center justify-start md:gap-6 border-t border-zinc-300 z-30"
-      role="tab-list"
       aria-label="Lista de partidos"
     >
       {parties.map(({ title, abbr }) => {
@@ -18,13 +17,10 @@ export function PartiesList({ parties, onClick }: PartiesListProps) {
           <Tabs.Trigger
             key={abbr}
             value={`tab-${abbr}`}
-            role="tab"
             data-testid={`tab-${abbr}`}
             id={`tab-${abbr}`}
             className="flex flex-col gap-1 items-center py-7 px-2 font-medium hover:text-white transition-colors duration-200 cursor-pointer"
-            aria-controls={`tab-partido-${abbr}`}
             onClick={() => onClick(abbr)}
-            // onClick={() => handlePartyClick(abbr)}
           >
             <strong>{abbr}</strong>
             <small className="uppercase">{title}</small>
