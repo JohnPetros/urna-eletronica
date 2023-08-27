f020f7d786f55d28950af9bca99caf77cdc07e2d
João Pedro <joaopedro.nc@outlook.com>
2023-08-27 14:00:03 -0300

    ♿ Adjust digit role

--- src/app/voting/components/Urn/Digit.tsx
+++ src/app/voting/components/Urn/Digit.tsx
@@ -10,13 +10,14 @@ interface DigitProps {
 
 export function Digit({ number, isActive }: DigitProps) {
   return (
-    <motion.div
+    <motion.span
       role="textbox"
       variants={blinkVariants}
       animate={isActive ? 'blink' : 'default'}
-      className="grid place-content-center border-2 border-zinc-900 w-8 h-10 font-extrabold text-lg peer"
+      className="grid place-content-center border-2 border-zinc-900 w-8 h-10 font-extrabold text-lg"
+      aria-labelledby="vonting-number"
     >
       {number}
-    </motion.div>
+    </motion.span>
   )
 }
