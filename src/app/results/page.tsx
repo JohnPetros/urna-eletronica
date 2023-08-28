946fe3fa751ec2dd8cb0ae4a473f7244895905c8
João Pedro <joaopedro.nc@outlook.com>
2023-08-28 19:36:42 -0300

    🐛 fix: Resolve location object

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -10,6 +10,7 @@ import { Vote } from './components/Vote'
 import { Variants, motion } from 'framer-motion'
 import { blinkAnimation } from '../voting/components/Urn/Display'
 import { useUser } from '@/hooks/useUser'
+import { useEffect } from 'react'
 
 const linkVariants: Variants = {
   hidden: {
@@ -36,35 +37,43 @@ export default function Results() {
     router.push('/')
   }
 
-  if (!state.votedCandidates.length) {
-    router.push('/voting')
-    return null
-  }
+  useEffect(() => {
+    if (!state.votedCandidates.length) {
+      router.push('/voting')
+    }
+  }, [])
 
-  return (
-    <div className="bg-blue-900 h-screen flex flex-col items-center">
-      <h2 className="text-zinc-100 text-2xl mt-3 text-center">Seus votos, {user?.name}:</h2>
-      <dl className="flex flex-col gap-8 mt-10">
-        {state.votedCandidates.map((candidate, index) => (
-          <Vote
-            key={`${candidate?.number}-${index}`}
-            role={ROLES_TITLES[index]}
-            candidate={candidate}
-            index={index}
-          />
-        ))}
-      </dl>
+  if (user && state.votedCandidates.length)
+    return (
+      <div className="bg-blue-900 h-screen flex flex-col items-center">
+        <h2 className="text-zinc-100 text-2xl mt-3 text-center">
+          Seus votos, {user?.name}:
+        </h2>
+        <dl className="flex flex-col gap-8 mt-10">
+          {state.votedCandidates.map((candidate, index) => (
+            <Vote
+              key={`${candidate?.number}-${index}`}
+              role={ROLES_TITLES[index]}
+              candidate={candidate}
+              index={index}
+            />
+          ))}
+        </dl>
 
-      <motion.button
-        variants={blinkAnimation}
-        animate={'blink'}
-        className="mt-8 text-zinc-100 bg-transparent uppercase font-semibold text-3xl tracking-wide"
-        onClick={handleExitClick}
-      >
-        <motion.span variants={linkVariants} initial="hidden" animate="visible">
-          Sair
-        </motion.span>
-      </motion.button>
-    </div>
-  )
+        <motion.button
+          variants={blinkAnimation}
+          animate={'blink'}
+          className="mt-8 text-zinc-100 bg-transparent uppercase font-semibold text-3xl tracking-wide"
+          onClick={handleExitClick}
+        >
+          <motion.span
+            variants={linkVariants}
+            initial="hidden"
+            animate="visible"
+          >
+            Sair
+          </motion.span>
+        </motion.button>
+      </div>
+    )
 }
--- src/components/PrivateRoute/index.tsx
+++ src/components/PrivateRoute/index.tsx
@@ -22,5 +22,5 @@ export function PrivateRoute({ children }: PrivateRouteProps) {
     registerUser(storagedUser)
   }, [])
 
-  return <>{children}</>
+  if (storagedUser) return children
 }
